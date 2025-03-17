562de1b6da22695674694c574b74feef9c095b9c
DrizzllDev <drizzllsvery@yandex.ru>
2025-03-17 12:28:19 +0300

    fix: добавил в сеттинг домен + добавил в гинигнор .idea

--- controlLinkAdmin/controlLinkAdmin/settings.py
+++ controlLinkAdmin/controlLinkAdmin/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+l5^&3t94y(y_c!u6agnb99*uu1^ely*#np8e3-k%j3dy&=+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] if DEBUG else ["link.razdorov.ru", "www.link.razdorov.ru"]
+CSRF_TRUSTED_ORIGINS = ["https://link.razdorov.ru/", "https://www.link.razdorov.ru/"]
 
 
 # Application definition
